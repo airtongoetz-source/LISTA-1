237968c716849b991bfc4a6868114ddad13e1d8a
Airton <airton_goetz@estudante.sesisenai.org.br>
2026-06-15 21:09:33 -0300

    q9

--- Program.cs
+++ Program.cs
@@ -1,29 +1,11 @@
-﻿Console.Write("Informe a primeira pontuação: ");
-int n1 = int.Parse(Console.ReadLine());
+﻿Console.Write("Informe um número inteiro: ");
+int numero = int.Parse(Console.ReadLine());
 
-Console.Write("Informe a segunda pontuação: ");
-int n2 = int.Parse(Console.ReadLine());
-
-Console.Write("Informe a terceira pontuação: ");
-int n3 = int.Parse(Console.ReadLine());
-
-if (n1 == n2 && n2 == n3)
+if (numero % 2 == 0)
 {
-    Console.WriteLine("As três pontuações são iguais.");
+    Console.WriteLine("O número informado é par.");
 }
 else
 {
-    int maior = n1;
-
-    if (n2 > maior)
-    {
-        maior = n2;
-    }
-
-    if (n3 > maior)
-    {
-        maior = n3;
-    }
-
-    Console.WriteLine($"Maior pontuação: {maior}");
+    Console.WriteLine("O número informado é ímpar.");
 }
